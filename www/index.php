e767866c147f57ea4bf9acb08920ca83d734141b
Michael Bertram <141644516+bertbigbite@users.noreply.github.com>
2025-10-23 20:53:17 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -8,6 +8,11 @@
 <body>
   <h1>Hello, World!</h1>
   <p>Welcome to your PHP Server</p>
+  <p>This page is served using PHP.</p>
+  <p>Visiting http://localhost/ now shows a list of all project folders in www/.</p>
+  <p><strong>Each subfolder can still be accessed directly:</strong><p>
+  <p>http://localhost/my_new_app</p>
+  <p>http://localhost/another_app</p>
   <?php
     echo "<p>Current date and time: " . date('Y-m-d H:i:s') . "</p>";
   ?>
